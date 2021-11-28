8d08a01822b5630d4f287e0387c94ecfc2717f2b
Christoph Kramer <christophkramer1@gmx.de>
2021-11-28 13:26:17 +0100

    added page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TestComponent } from './test/test.component';
+import { RecepyComponent } from './recepy/recepy.component';
 
 const routes: Routes = [
-  {path: '', component: TestComponent}
+  {path: '', component: TestComponent},
+  {path: 'recepy', component: RecepyComponent}
 ];
 
 @NgModule({
